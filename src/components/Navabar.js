990fdbd9629a6ed268c06010f1046f90fc8d8a97
Tj_ <tjvishwa88@gmail.com>
2023-05-13 18:54:24 +0530

    "Component - Navbar implemented"

--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -1,40 +1,32 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Navabar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link className="navbar-brand" to='/'>Blogger</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
+                    <Link className="nav-link" to='/'>Link</Link>
                     </li>
                     <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <Link className="nav-link dropdown-toggle" to='/' id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </Link>
+                    
                     </li>
                 </ul>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <button type="button" className="btn  mx-2 btn-light">Login</button>
+                <button type="button" className="btn  btn-light">Sign Up</button>
                 </div>
             </div>
             </nav>
@@ -42,4 +34,4 @@ const Navabar = () => {
   )
 }
 
-export default Navabar
+export default Navabar;
