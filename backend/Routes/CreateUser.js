cc893e56a95c909689d7877c930eb65a53fb534c
Tj_ <tjvishwa88@gmail.com>
2023-05-14 13:40:51 +0530

    "feat - validations added for user sign up"

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const {body, validationResult} = require('express-validator');
 
-router.post('/createuser', async (req,res)=>{
-   try{
-     await User.create({
-            name:req.body.name,
-            password:req.body.password,
-            email:req.body.email,
+router.post('/createuser',[
+        body('username','Incorrect email').isEmail(),
+        body('name','Name should have more than 5 chars').isLength(),
+        body('password','Incorrect password').isLength({min:5})] 
+        ,async (req,res)=>{
 
-      })
-      res.json({success:true});
-   }catch(error){
-      console.log(error);
-      res.json({success:false});
-   }
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).json({ errors: errors.array()})
+            }
+        try{
+            await User.create({
+                    name:req.body.name,
+                    password:req.body.password,
+                    email:req.body.email,
+
+            })
+            res.json({success:true});
+        }catch(error){
+            console.log(error);
+            res.json({success:false});
+        }
 })
 
 
