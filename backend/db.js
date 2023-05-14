6550f91de8cd510fabf2222fe0718ec99ac6e9eb
Tj_ <tjvishwa88@gmail.com>
2023-05-14 13:05:52 +0530

    "feature - User model set up and signUp created backend"

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://Blogger:ukQ0LPB7xQEods1f@cluster0.k7vpspy.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://Blogger:ukQ0LPB7xQEods1f@cluster0.k7vpspy.mongodb.net/BloggerData?retryWrites=true&w=majority';
 const mongoDB = ()=>{
   mongoose.connect(mongoURI,{useNewUrlParser : true})
         .then((res)=>{
